cf686a5fe3144fa34c78b130693a9fbc2706a448
pon0204 <tsuyoshi.gonzaresu.0204@gmail.com>
2021-05-21 14:02:58 +0900

    テキスト保存処理

---
+++ src/indexeddb/memos.ts
@@ -0,0 +1,19 @@
+import Dexie from 'dexie'
+
+
+// tyepscriptの型定義
+export interface MemoRecord {
+  datetime: string
+  title: string
+  text: string
+}
+
+// データベース名
+const database = new Dexie('markdown-editor')
+database.version(1).stores({ memos: '&datetime' })
+const memos: Dexie.Table<MemoRecord, string> = database.table('memos')
+
+export const putMemo = async (title: string, text: string): Promise<void> => {
+  const datetime = new Date().toISOString()
+  await memos.put({ datetime, title, text })
+}
